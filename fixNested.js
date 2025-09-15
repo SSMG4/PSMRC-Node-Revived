2e443aba3e2f9e310219a29ff2c42cfd810d34f2
SSMG4 <matthew.gallon0912@gmail.com>
2025-09-16 00:17:42 +0200

    Update Paths

--- fixNested.js
+++ fixNested.js
@@ -2,14 +2,14 @@ var fs = require('fs-extra');
 
 module.exports = function(cookie) {
   return new Promise( (resolve, reject) => {
-    var files = fs.readdirSync(__dirname + '/uploads/' + cookie + '-unzip')
+    var files = fs.readdirSync(__dirname + 'uploads/' + cookie + '-unzip')
     var bestCandidate = '';
 
     if (files.indexOf('assets') === -1) {
       
       // Find the best candidate for being the directory we need
       files.forEach( file => {
-        if (fs.lstatSync(__dirname + '/uploads/' + cookie + '-unzip/' + file).isDirectory()) {
+        if (fs.lstatSync(__dirname + 'uploads/' + cookie + '-unzip/' + file).isDirectory()) {
           console.log('setting best candidate');
           bestCandidate = file;
         }
@@ -19,7 +19,7 @@ module.exports = function(cookie) {
       // if (bestCandidate === '' && files.length === 1 && files[0] === '.DS_Store') {reject()}
 
       // Move files
-      var mvFileList = fs.readdirSync(__dirname + '/uploads/' + cookie + '-unzip/' + bestCandidate);
+      var mvFileList = fs.readdirSync(__dirname + 'uploads/' + cookie + '-unzip/' + bestCandidate);
       mvFileList.forEach( file => {
         var oldPath = __dirname + '/uploads/' + cookie + '-unzip/' + bestCandidate + '/' + file;
         var newPath = __dirname + '/uploads/' + cookie + '-unzip/' + file;
